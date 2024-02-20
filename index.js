d66b29a4fca1c68295e308076a30f59b5de94b15
Pahadi18bhutiya <uniyalparas2005@gmial.com>
2024-02-20 16:39:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,451 @@
+// var a="hi klkl";
+
+// var b=prompt("Whats your age");
+// if(b>=18){
+//     console.log("you can drive car");    
+// }
+// else{
+//     console.log("you can not drive car");
+// }
+// console.log(b.length);
+
+// function xy( a, c){
+ 
+//     var b=a.slice(0,1);
+//     // console.log(b);
+    
+//     console.log(b.toUpperCase()+a.slice(1,5));
+// }
+
+// var a="Paras";
+// let c="paras";
+// xy(a,c);
+
+// Arrow function 
+// var a=()=>{
+//     var a="Paras";
+//     let c="paras";
+//     var b=a.slice(0,1);
+//     // console.log(b);
+    
+//     console.log(b.toUpperCase()+a.slice(1,5)+" With arrow Function");
+// }
+
+// var array=[1,2,3,4,2];
+// {}
+// console.log(array);
+// array.push(5);
+// console.log(array);
+// array.pop()
+// console.log(array);
+// array.sort();
+// console.log(array);
+
+// var c=4;
+
+// var b=Math.random()*5;
+// console.log(b);
+// var c=Math.floor(b);
+// console.log(c);
+
+// var i=3;
+
+// do{
+//     console.log(i);
+//     i++;
+//     }while(i==3)
+// function func(){
+//     var b=document.form.inp.value;
+//     var c=document.form.imp.value;
+//     var d=parseInt(b);
+//     var e=parseInt(c);
+//     var f=d+e;
+    
+//     document.querySelector('#print').innerHTML=f;
+
+//     // document.getElementsByTagName('p')[0].innerHTML="hi"
+//     // document.getElementsByTagName('p')[1].innerHTML="hi i am "
+// }
+// function func(){
+//     var a=Math.floor(Math.random()*11);
+//     var b=document.querySelectorAll('p')[0].innerHTML=a;
+// }
+
+// spreed operatorsv 
+// var a=[1,2,3,4];
+// var b=[...a];
+
+// b.push(5);
+// console.log(a);
+// console.log(b);
+
+
+// destructuring 
+// const [car, ,suv] = ['mustang', ' ', 'expedition'];
+
+// console.log(car+' '+suv);
+
+// function start(){
+//     let a=10;
+//     var b=20;
+//     function dd(){
+//         console.log(d);
+//     }
+//     dd()
+//     const c=30;
+//     console.log(a);
+// }
+
+
+// var d=80;
+// start();
+
+// console.log(b);
+
+
+
+
+
+
+// pAras 
+
+
+
+// hoisting 
+// /lp();
+
+// function lp(){
+//     console.log('hi i M UNDER WATER');
+// }
+
+// var c=null;
+// console.log(c);        
+// var a=10 , b=12;
+// if( a>b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+// var a =document.getElementById(k);
+
+// console.log(a);
+
+
+
+// var a=[1,2,4];
+// for (let i = 0; i < a.length; i++) {
+//    console.log(a[i]);
+    
+// }
+
+
+// DOM 
+// concatination 
+// 10p
+
+// 1010p 
+// 20p
+
+//p1010
+
+// Nan 
+
+// Not a Number
+
+//null undefined
+
+
+
+// question 
+
+// how to create empty array
+// q1 
+//  var a=[];
+//  a.push(1);
+//  var b=[null];
+//  console.log(b);
+
+// q2
+// How do you access the first and last elements of an array?
+// var a=[1,2,3,4,5];
+// for (let i = 0; i < a.length; i++) {
+//     if(a[i]==a[a.length-1]){
+//         console.log(a[i]);     
+
+//     }
+// }
+// console.log(a[0]);
+// for (let i = 0; i<a.length; i++) {
+//     if (a[i]=a[0]) {
+//         console.log(a[i]); 
+        
+//     }
+    
+// }
+// q3
+// How do you add an element to the end of an array
+// var a=[1,2,3,4,5];
+// a.push(6);
+// console.log(a);
+
+// q4
+// How do you loop through an array in JavaScript?
+// var a=[1,2,3,4,5,6,7,8,9];
+// for (let i = 0; i <= a.length-1; i++){
+//     console.log(a[i]);
+// }
+
+// q5
+// How do you check if an element exists in an array?
+// var a=[1,2,3,4,5,6,7,8,9,];
+// var n=5;
+// for (let i =0 ; i < a.length;i++) {
+// if(a[i]==n){
+//     console.log(a[i]);
+// }
+// }
+
+// q6
+// How do you remove an element from an array at a specific index?
+// var a=[1,2,3,4,5];
+// a.pop(5);
+// console.log(a);
+
+// q7
+// How do you concatenate two arrays in JavaScript?
+// var a=[1,2,3,4,5];
+// var b=[,6,7,8,9];
+// var c=a+b;
+// console.log(c);
+
+// q8
+// Write a function to flatten a nested array in JavaScript
+// var a=[1,[2,[3,4],5],6];
+// console.log(a);
+
+// q9
+// How do you sort an array in JavaScript?
+// var a=[9,5,4,7,6,2,3,1];
+// a.sort();
+// console.log(a);
+
+// q1 C Program to Check Whether a Number is Positive, Negative, or Zero
+// q2 JS Program to Check Whether Number is Even or Odd
+// q3 JS Program to Check Whether a Character is Vowel or Consonant 
+// q4 JS Program to Find Largest Number Among Three Numbers
+// q5 JS Program to Calculate Sum of Natural Numbers
+// sol 1
+// var a=4;
+// if(a>0){
+//     console.log("a is positive no");
+// }
+// else{
+//     console.log("a is negetive no");
+// }
+
+// sol 2/
+// var a=5;
+// if(a%2==0){
+//     console.log("a is even no");
+// }
+// else{
+//     console.log("a is odd no");
+// }
+
+// sol 3
+// var a='o';
+// if(a=='a'||a='e'||a='i'||a='o'||a='u'){
+//     console.log("the character is an vovel");
+// }
+// else{
+//     console.log("the character is consonent");
+// }
+
+// sol 4
+// var a=4;
+// var b=5;
+// var c=6;
+// if(a>b&&a>c){
+//     console.log("a is the graters no");
+// }
+// else if(b>a&&b>c){
+//     console.log("b is the gratest no");
+// }
+// else{
+//     console.log("c is the gratest no");
+// }
+
+// sol 5
+// var a=10;      //15 1+2+3+4+5
+// var count=0;
+// for(let i=0;i<=a;i++){
+//      count=count+i;
+// }
+// console.log(count);
+
+//q1 C Program to Check Leap Year
+//q2 C Program to Find Factorial of a Number
+//q3 C Program to Make a Simple Calculator 
+//q4 C Program to Generate Multiplication Table 
+//q5 C Program to Print Fibonacci Series
+
+// sol 1
+// var a=2004;
+// if(a%4==0||a%100==0||a%400==0){
+//     console.log(+a" is a leap yaer");
+// }
+// else{
+//     console.log(+a" is not a leap year");
+// }
+
+// sol 2
+// var a=10;      //15 1+2+3+4+5
+// var count=0;
+// for(let i=0;i<=a;i++){
+//      count=count+i;
+// }
+// console.log(count);
+
+// sol 3
+// program for a simple calculator
+
+// take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// let result;
+
+// // using if...else if... else
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+// // display the result
+// alert(`${number1} ${operator} ${number2} = ${result}`);
+
+// sol 4
+// var a=5;
+// var count;
+// for(let i=1 ;i<=10;i++){
+//     count=a*i;
+//     console.log(count);
+// }
+
+// sol 5
+// var n1=0;
+// var n2=1;
+// var nextno;
+// for(let i=0 ;i<=10;i++){
+//     console.log(n1);
+//     nextno=n1+n2;
+//     n1=n2;
+//     n2=nextno;
+// }
+// for each loop 
+// var a=[1,2,3,4,5,6,7,8,9,10];
+// const k=a.forEach((b)=>{
+//     console.log(b*2);
+//     return b;
+// })
+// console.log(k);
+// map 
+// var a=[1,5,8,9,7];
+// const k=a.map((b)=>{
+//     return b*2;
+// })
+// console.log(k);
+// filter 
+// let a=[1,2,3,4,5,6,7,8,9];
+// const evenno=a.filter((num)=>{
+//     return num%2==0;
+// })
+// const oddno=a.filter((num)=>{
+//     return num%2!=0;
+// })
+// console.log(oddno);
+//  var b= prompt('enter the ');
+ 
+//  if(b=='oddno'){
+//     console.log(oddno);
+//  }else if(b=='evenno'){
+//     console.log(evenno);
+//  }else{
+//     console.log("error");
+//  }
+// var b=[{
+//     name:'paras',
+//     id:'101',
+//     marks:1000
+// },
+// {
+//     name:'shivam',
+//     id:'102',
+//     marks:999
+// },
+// {
+//     name:'pankaj',
+//     id:'103',
+//     marks:401
+// },
+// {
+//     name:'mohit',
+//     id:'104',
+//     marks:500
+// },
+
+// ]
+// // const k=b.map((mar)=>{
+// //     return mar.marks;
+// // })
+// // console.log(k);
+// const k=b.filter((data)=>{
+//     return data.marks>500;
+// })
+// console.log(k);
+
+// async function fun(){
+
+
+
+//  const url = 'https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+// 		'X-RapidAPI-Host': 'ronreiter-meme-generator.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+// }
+// fun();
+async function generate(){
+    const api=`https://meme-api.com/gimme/wholesomememes`;
+    const data=await fetch(`${api}`).then(Response=>Response.json());
+    
+    document.querySelector('h4').innerHTML=data.title;
+    var image=data.url;
+    document.querySelector('img').setAttribute("src",image);
+    document.querySelector('h5').innerHTML= "Author: " + data.author;
+
+}
